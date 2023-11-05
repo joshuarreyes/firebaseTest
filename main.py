bb4f552c0b5b8fede25f8eef782474534d24b084
Joshua Reyes <jjsms0759@gmail.com>
2023-11-05 00:44:27 -0400

    add methods to sign in, sign up, authenticate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyrebase
-
+import requests
+import json
 
 firebaseConfig = {
   "apiKey": "AIzaSyCRKlOgiKat20IdHzolR012qFffMek9vKU",
@@ -16,7 +17,22 @@ firebaseConfig = {
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
 
-def authenticate_user(email, password):
+def processHttpError(e):
+    """
+    Process HTTP error.
+
+    Args:
+        e (requests.HTTPError): HTTP error.
+
+    Returns:
+        None
+    """
+    error_json = e.args[1]
+    error = json.loads(error_json)['error']
+    print("Error:", error['message'])
+    return None
+
+def authenticate_user(user):
     """
     Authenticate user with email and password.
 
@@ -25,14 +41,14 @@ def authenticate_user(email, password):
         password (str): User's password.
 
     Returns:
-        dict: User's authentication token and other details.
+        True if authentication is successful, False otherwise.
     """
     try:
-        user = auth.sign_in_with_email_and_password(email, password)
         auth.send_email_verification(user['idToken'])
-        return user
+        return True
     except Exception as e:
         print("Authentication failed")
+        return False
 
 def signup():
     """
@@ -45,12 +61,77 @@ def signup():
         email = input("Enter your email: ")
         password = input("Enter your password: ")      
         user = auth.create_user_with_email_and_password(email, password)
-        print("Signup successful!")
-        authenticate_user(email, password)
+        print("Signup successful, verify your email to use app.")
+        authenticate_user(user)
         return user
-    except Exception as e:
-        print("Signup failed: ", e)
+    except requests.HTTPError as e:
+        return processHttpError(e)
 
-print(signup())
+def signin():
+    """
+    Sign in an existing user.
+
+    Returns:
+        dict: User's authentication token and other details.
+    """
+    try:
+        email = input("Enter your email: ")
+        password = input("Enter your password: ")      
+        user = auth.sign_in_with_email_and_password(email, password)
+        print("Signin successful!")
+        return user
+    except requests.HTTPError as e:
+        return processHttpError(e)
+
+def emailVerified(user):
+    """
+    Check if user's email is verified.
+
+    Args:
+        user (dict): User's authentication token and other details.
+
+    Returns:
+        bool: True if email is verified, False otherwise.
+    """
+    if user is None:        
+        return False
+    try:
+        if auth.get_account_info(user['idToken'])['users'][0]['emailVerified']:
+            return True
+        else:
+            return False
+    except requests.HTTPError as e:
+        return processHttpError(e)
+    
+def main():
+    """
+    Main function.
+    """
+    running = True
+    user = None
+    while running:
+        print("1. Sign up")
+        print("2. Sign in")
+        print("3. Exit")
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
+            user = signup()
+        elif choice == 2:
+            user = signin()
+
+            if user is None:
+                continue
+
+            if emailVerified(user):
+                print("Email verified!")
+            else:
+                print("Email not verified!")
+                user = None
+        elif choice == 3:
+            running = False
+        else:
+            print("Invalid choice!")
 
 
+if __name__ == "__main__":
+    main()
